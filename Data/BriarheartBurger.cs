1e2dfd86a5d2687ba6d401ccdda1932298c47925
Jonathan Welch <jonathanthebetter@ksu.edu>
2020-08-26 14:43:04 -0500

    Done with Brairheart and soda

---
+++ Data/BriarheartBurger.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Author: Jonathan Welch
+ * Class name: Briarheart Burger.cs
+ * Purpose: Class used to represent the briarheart burger
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data.Entrees
+{
+    public class BriarheartBurger
+    {
+        private double price = 0;
+        private uint calories = 0;
+        private List<string> instructions;
+        private bool bun = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+        private bool pickle = true;
+        private bool cheese = true;
+
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { pickle = value; }
+        }
+
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        public bool Mustard
+        { get { return mustard; } set { mustard = value; } }
+
+        public bool Bun
+        {
+            get { return bun; }
+            set { bun = value; }
+        }
+        public bool Ketchup
+        {
+            get { return ketchup; }
+            set { ketchup = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = 6.32; }
+        }
+
+        public uint Calories
+        {
+            get { return calories; }
+            set { calories = 843; }
+        }
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                instructions = new List<string>();
+                if (!bun)
+                {
+                    instructions.Add("Hold bun");
+                }
+                if (!ketchup)
+                {
+                    instructions.Add("Hold ketchup");
+                }
+                if (!mustard)
+                {
+                    instructions.Add("Hold mustard");
+                }
+                if (!pickle)
+                {
+                    instructions.Add("Hold pickle");
+                }
+                if (!cheese)
+                {
+                    instructions.Add("Hold cheese");
+                }
+                return instructions;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Briarheart Burger";
+        }
+    }
+}
---
+++ Data/Enums/Sodaflavor.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Author: Jonathan Welch
+ * Class name: SodaFlavor.cs
+ * Purpose: Class used to represent sodaflavor through an enumeration
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data.Enums
+{
+    public enum Sodaflavor
+    {
+        Blackberry,
+        Cherry,
+        Grapefruit,
+        Lemon,
+        Peach,
+        Watermelon
+    }
+}
